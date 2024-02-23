0b4d1e5331853685674aa4688769430ae0c43824
Lorenzo-Rios <lorenzorios2003@gmail.com>
2024-02-23 11:01:38 -0300

    s

--- js/uiLogic.js
+++ js/uiLogic.js
@@ -1,9 +1,3 @@
-document.querySelector('#button-register').addEventListener('click', saveUser);
+document.querySelector('#button-register').addEventListener("click", saveUser)
 
-function saveUser (){
-    const sUser = document.querySelector('#user-tag').value,
-          sEmail = document.querySelector('#correo-usuario').value;
-    
-    addUser(sUser, sEmail);
-    
-}
+saveUser(pname, pemail, )
