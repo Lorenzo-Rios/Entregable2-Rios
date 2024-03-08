78b7acefb361802891b98457fc886556185f2a95
Lorenzo-Rios <lorenzorios2003@gmail.com>
2024-03-08 02:43:02 -0300

    Error de tipografia (aproved => approved).

--- client/js/appi.js
+++ client/js/appi.js
@@ -1,16 +1,15 @@
-
 const mp = new MercadoPago('TEST-880b6ef0-f4cd-4028-b418-d967512f8880', {
     locale: "es-AR",
 });
 
-document.getElementsByClassName("btn-pagar")[0].addEventListener("click", async () =>{
-    try{
+document.getElementsByClassName("btn-pagar")[0].addEventListener("click", async () => {
+    try {
         const orderData = {
             title: "Tu producto",
             quantity: 1,
             price: 100
         };
-    
+
         const response = await fetch("http://localhost:3000/create_preference", {
             method: "POST",
             headers: {
@@ -18,12 +17,12 @@ document.getElementsByClassName("btn-pagar")[0].addEventListener("click", async 
             },
             body: JSON.stringify(orderData),
         });
-        
+
         const preference = await response.json();
         createCheckoutButton(preference.id);
 
-    } catch(error){
-        alert("error(el servidor no esta corriendo)");
+    } catch (error) {
+        alert("Error: El servidor no está corriendo");
     }
 });
 
@@ -31,14 +30,14 @@ const createCheckoutButton = (preferenceId) => {
     const bricksBuilder = mp.bricks();
 
     const renderComponent = async () => {
-        if(window.checkoutButton) window.checkoutButton.unmount();
+        if (window.checkoutButton) window.checkoutButton.unmount();
         await bricksBuilder.create("wallet", "wallet_container", {
             initialization: {
                 preferenceId: preferenceId,
             },
             customization: {
                 texts: {
-                 valueProp: 'smart_option',
+                    valueProp: 'smart_option',
                 },
             },
         });
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { MercadoPagoConfig, Preference } from "mercadopago";
+import { MercadoPagoConfig } from "mercadopago";
+import { Preference } from "mercadopago";
 
 const client = new MercadoPagoConfig({ accessToken: 'TEST-905919541089682-030219-a837a742bd14ee8f7878ba223f9d15b2-186113869'});
 
@@ -8,15 +9,14 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send("Soy el server");
+app.get("/", (req, res) => {
+    res.send("Soy el servidor");
 });
 
-app.post("/create_preference", async (req, res)=>{
-    try{
+app.post("/create_preference", async (req, res) => {
+    try {
         const body = {
             items: [
                 {
@@ -31,21 +31,22 @@ app.post("/create_preference", async (req, res)=>{
                 failure: "https://www.instagram.com/lolorioss_/",
                 pending: "https://www.instagram.com/lolorioss_/",
             },
-            auto_return: "aproved",
+            auto_return: "approved",  
         };
+
         const preference = new Preference(client);
         const result = await preference.create({ body });
         res.json({
             id: result.id,
         });
-    }catch (error) {
+    } catch (error) {
         console.log(error);
         res.status(500).json({
             error: "Error al crear la preferencia :("
         });
-    };
+    }
 });
 
-app.listen(port, ()=>{
-    console.log(`el servidor corre en el puerto ${port}`);
+app.listen(port, () => {
+    console.log(`El servidor corre en el puerto ${port}`);
 });
